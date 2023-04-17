c388e9de19c00a327d97584296b6551a5bcbd224
imran <115683643+itsimran007@users.noreply.github.com>
2023-04-17 12:54:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let circle1 = document.getElementById('circle1');
+circle1.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'lightblue';
+})
+
+let circle2 = document.getElementById('circle2');
+circle2.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'gray';
+})
+
+let circle3 = document.getElementById('circle3');
+circle3.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'yellowgreen';
+})
+
+let circle4 = document.getElementById('circle4');
+circle4.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'peru';
+})
